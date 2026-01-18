0fe6b6ae3d85c5c1682d5f59088d19755fac4038
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-18 12:46:01 -0300

    adiciona page register

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { registerService } from '@/src/services/register-service';
+import { PathLinks } from '@/src/types/path-links';
+import { RegisterForm, registerFormSchema } from '@/src/types/register-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { redirect } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+
+
+
+export default function RegisterPage() {
+  const { register, handleSubmit, formState: { errors, isSubmitting},} = useForm<RegisterForm>({
+    resolver:zodResolver(registerFormSchema)
+  });
+
+  const onSubmit = async (formData:RegisterForm) => {
+    const {success} = await registerService(formData);
+
+    if(!success) {
+        alert('Falha ao criar conta!');
+    }
+    alert('Conta criada com sucesso!');
+    redirect(PathLinks.LOGIN);
+  };
+
+
+  return (
+    <div className="w-full h-[91vh] flex items-center justify-center main bg-white">
+      <form action="POST" className='w-full max-w-md h-[450px] p-6 bg-white shadow-md border border-gray-200 rounded-[12px]' onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-2xl font-bold mt-8 text-black">Crie sua conta</h1>
+          <p className="text-gray-500 mb-2">Crie uma conta simples e rápido!</p>
+          <input
+            type="text"
+            placeholder="Nome do usuário"
+            className="border border-gray-400 text-gray-600 p-2 rounded-md w-64"
+            {...register("name")}
+          />
+          {errors.name && (
+            <span className='text-red-500 text-sm'>{errors.name.message}</span>
+          )}
+          <input
+            type="email"
+            placeholder="E-mail"
+            className="border border-gray-400 text-gray-600 p-2 rounded-md w-64"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className='text-red-500 text-sm'>{errors.email.message}</span>
+          )}
+          <input
+            type="password"
+            placeholder="Senha"
+            className="border border-gray-400 text-gray-600 p-2 rounded-md w-64"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="text-red-500 text-sm">{errors.password.message}</span>
+          )}
+          <button
+            type="submit"
+            className="bg-black text-white w-60 px-6 py-2 mt-2 rounded-[20px] hover:bg-zinc-900 transition-colors cursor-pointer"
+            disabled={isSubmitting}
+          >
+           { isSubmitting ? 'Criando conta...' : 'Criar conta' }
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
